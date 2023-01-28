40584875dd1c33c351dca5785ca471f4a4b000e7
Corey Walker <corey.walker@veterantech.io>
2023-01-28 08:36:01 -0500

    Added notify.yml and notify_start.yml

---
+++ jiraProxy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# ./jiraProxy.sh build|deployment <file>
+
+curl \
+-w "%{http_code}" \
+-H "Content-Type: application/json" \
+-H "Accept: application/json" \
+-X POST "https://circleci.com/api/v1.1/project/gh/CircleCI-Public/jira-connect-orb/jira/${1}?circle-token=${CIRCLE_TOKEN}" --data @$2
