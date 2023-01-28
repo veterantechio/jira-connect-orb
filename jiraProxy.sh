ee626a237b639c508aaeab13d0b9f3d0b13f4e81
Corey Walker <corey.walker@veterantech.io>
2023-01-28 09:30:47 -0500

    Fixes for build errors

--- jiraProxy.sh
+++ jiraProxy.sh
@@ -6,4 +6,4 @@ curl \
 -w "%{http_code}" \
 -H "Content-Type: application/json" \
 -H "Accept: application/json" \
--X POST "https://circleci.com/api/v1.1/project/gh/CircleCI-Public/jira-connect-orb/jira/${1}?circle-token=${CIRCLE_TOKEN}" --data @$2
+-X POST "https://circleci.com/api/v1.1/project/gh/CircleCI-Public/jira-connect-orb/jira/${1}?circle-token=${CIRCLE_TOKEN}" --data @"$2"
